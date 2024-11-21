7c3953a02c133a5fd1c871c2c8a72d514192cb33
ArthurErlich <a.erlich@live.de>
2024-11-21 14:14:03 +0100

    basic canvas setup

--- game.js
+++ game.js
@@ -1,20 +1,19 @@
+"use strict";
 /// <reference path="node_modules/excalibur/build/dist/excalibur.d.ts" />
-
-const game = new ex.Engine({
+var game = new ex.Engine({
     width: 300,
     height: 300,
     canvasElementId: 'game'
 });
-const basicActor = new ex.Actor({
+var basicActor = new ex.Actor({
     name: "basicActor",
     width: 50,
     height: 30,
     color: ex.Color.Chartreuse
 });
-basicActor.actions.repeatForever((builder => {
+basicActor.actions.repeatForever((function (builder) {
     builder.moveBy(ex.vec(100, 100), 10);
     builder.moveBy(ex.vec(-100, -100), 10);
-
 }));
 game.add(basicActor);
 game.start();
