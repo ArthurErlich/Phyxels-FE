05c62d8edfe9619d6d2c86ae2fc0c88a01950540
ArthurErlich <Arthur.Erlich@zgs-konstanz.de>
2024-11-22 11:42:51 +0100

    src/actors/Element.ts aktualisiert

--- src/actors/Element.ts
+++ src/actors/Element.ts
@@ -1,5 +1,8 @@
 namespace phyxels{
     export abstract class Element extends ex.Actor{
-        
+        /**
+         * onPreUpdate  ():void {}
+         * onPostUpdate ():void {}
+         */
     }
 }
