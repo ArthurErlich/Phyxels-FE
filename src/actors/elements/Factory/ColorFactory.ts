b2e4c1db846bc58c6f8f2aed582c13a4ef48c0d3
ArthurErlich <a.erlich@live.de>
2025-03-03 17:30:27 +0100

    made path relativ

--- src/actors/elements/Factory/ColorFactory.ts
+++ src/actors/elements/Factory/ColorFactory.ts
@@ -3,7 +3,7 @@ namespace phyxels {
         private static colors: any;
 
         public static loadColors(): void {
-            fetch('/src/configs/colors.json')
+            fetch('../../../configs/colors.json')
                 .then(response => {
                     if (!response.ok) {
                         throw new Error(`Failed to load colors.json: ${response.statusText}`);
